ed734787ef104190dbc3e7effb443754447b6ee8
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-10-01 13:05:21 -0300

    Update Cpp_AsmII.cpp

--- Cpp_AsmII.cpp
+++ Cpp_AsmII.cpp
@@ -4,9 +4,9 @@ int add(){
 	
 	int result;
 
-	__asm__ ("movl $5, %%eax;"
-			"movl $3,%%ebx;"
-			"addl %%ebx, %%eax;" :"=a" (result));
+	__asm__ ("movl $5, %%eax;" //Move 5 pra eax
+		"movl $3,%%ebx;" //Move 3 pra ebx
+		"addl %%ebx, %%eax;" :"=a" (result)); //Soma ebx + eax eax= 8; Armazena o resultado em result.
 
 return result;
 
