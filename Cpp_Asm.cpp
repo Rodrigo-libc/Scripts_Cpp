ef172623d0eeafdf5c4649f1d68e18e52a06fc85
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-10-01 12:42:48 -0300

    Add files via upload

---
+++ Cpp_Asm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int add(int a, int b){
+	
+	int result;
+
+	__asm__ ("addl %%ebx, %%eax;"
+			:"=a" (result)
+			: "a" (a), "b" (b)
+			);
+
+return result;
+
+}
+
+int main()
+{
+
+	int sum = add(10, 20);
+	
+	std::cout << "Soma: " <<sum << std::endl;
+
+	return 0;
+}
