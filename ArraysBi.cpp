9b60397e22813700eead0a5ab29b72a605f25205
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-10-01 20:03:35 -0300

    Update ArraysBi.cpp

--- ArraysBi.cpp
+++ ArraysBi.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	int Numeros[3][4]={{1,2,3,4},
-					   {5,6,7,8},
-					   {9,10,11,12}
-					 };
+			   {5,6,7,8},
+			   {9,10,11,12}
+			};
 
 	cout <<"Acessando elemento da linha 2 coluna 3 -> " 
 		 <<Numeros[1][2] <<endl;
