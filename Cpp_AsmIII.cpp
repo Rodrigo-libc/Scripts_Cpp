2f71414d080af866d482104d3aada6ba9127601a
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-10-01 13:44:24 -0300

    Update Cpp_AsmIII.cpp

--- Cpp_AsmIII.cpp
+++ Cpp_AsmIII.cpp
@@ -4,11 +4,11 @@ int add(int a, int b){
 	
 	int result;
 
-	__asm__ ("movl %[input_a], %%eax;"
-			"addl %[input_b], %%eax;"
-			 : "=a" (result)
-			 : [input_a] "r" (a), [input_b] "r" (b)
-			 );
+	__asm__ ("movl %[input_a], %%eax;" //Move parâmetro (a) para eax
+		"addl %[input_b], %%eax;" //Soma parâmetro (b) a eax
+		: "=a" (result) //Armazena a soma em result
+		: [input_a] "r" (a), [input_b] "r" (b) //Parâmetros sendo passados para as varíaveis...
+		);
 
 return result;
 
